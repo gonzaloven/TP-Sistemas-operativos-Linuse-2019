022cac72be1d710f6e31775929382aa6f895b1b1
FaridUTN <faridfafu@hotmail.com>
2019-11-29 21:17:03 -0300

    Cambios en hilolay_alumnos

--- libs/Hilolay/hilolay_alumnos.c
+++ libs/Hilolay/hilolay_alumnos.c
@@ -1,21 +1,40 @@
-#include "hilolay_alumnos.h"
-#include <utils_suse/libraries.h>
+//#include <hilolay_alumnos.h>
+#include <libraries.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define HILOLAY_ALUMNO_CONFIG_PATH "../configs/hilolay_alumnos.config"
+
 //todo no se si cada vez que esperamos una respuesta hay que fijarse de que cop viene por las dudas
 
-int socket_suse = 0; //todo verificar si hay que conectarse aca en la global o basta con hacerlo en la funcion
-int master_thread = 0;
+typedef struct hilolay_operations {
+	int (*suse_create) (int);
+	int (*suse_schedule_next) (void);
+	int (*suse_join) (int);
+	int (*suse_close) (int);
+	int (*suse_wait) (int,char*);
+	int (*suse_signal) (int,char*);
+} hilolay_operations;
 
-char* log_path = "../logs/hilolay_alumnos_logs.txt";
-t_log* logger = log_create(log_path, "Hilolay Alumnos Logs", 1, 1);
+hilolay_operations *main_ops;
+typedef struct hilolay_alumno_configuracion {
+	char* SUSE_IP;
+	char* SUSE_PORT; //Era int, lo paso a char* como lo pide la funcion de conectar_a
+} hilolay_alumnos_configuracion;
+
+hilolay_alumnos_configuracion * configuracion_hilolay;
+
+int socket_suse; //todo verificar si hay que conectarse aca en la global o basta con hacerlo en la funcion
+int master_thread;
+
+//	char* log_path = "../logs/hilolay_alumnos_logs.txt";
+//	t_log* logger = log_create(log_path, "Hilolay Alumnos Logs", 1, 1);
 
 void conectar_con_suse() {
-	socket_suse = conectar_a(configuracion_hilolay.SUSE_IP ,configuracion_hilolay.SUSE_PORT);
-	log_info(logger, "Me conecte con SUSE por el socket %d. \n", socket_suse);
+	int socket_suse = conectar_a(configuracion_hilolay->SUSE_IP ,configuracion_hilolay->SUSE_PORT);
+//	log_info(logger, "Me conecte con SUSE por el socket %d. \n", socket_suse);
 
 	int tamanio_buffer = sizeof(int);
 	void * buffer = malloc(tamanio_buffer);
@@ -23,11 +42,11 @@ void conectar_con_suse() {
 
 	serializar_int(buffer, &desp, 0);
 	enviar(socket_suse, cop_handshake_hilolay_suse, tamanio_buffer, buffer);
-	log_info(logger, "Envie un mensaje (0) a SUSE. \n");
+//	log_info(logger, "Envie un mensaje (0) a SUSE. \n");
 }
 
 int suse_create(int master_thread){
-	log_info(logger, "Ejecutando suse_create... \n");
+//	log_info(logger, "Ejecutando suse_create... \n");
 	bool result = realizar_handshake(socket_suse, cop_suse_create);
 
 
@@ -38,15 +57,15 @@ int suse_create(int master_thread){
 	serializar_int(buffer, &desp, master_thread);
 	enviar(socket_suse, cop_generico, tamanio_buffer, buffer);
 	free(buffer);
-	log_info(logger, "Envie el tid %d a suse. \n");
-	log_info(logger, "La respuesta de suse_create es %d \n", (int)result);
+//	log_info(logger, "Envie el tid %d a suse. \n");
+//	log_info(logger, "La respuesta de suse_create es %d \n", (int)result);
 
 	return (int)result; //todo responder correctamente
 
 }
 
 int suse_schedule_next(){ //todo testear
-	log_info(logger, "Ejecutando suse_schedule_next %d... \n");
+//	log_info(logger, "Ejecutando suse_schedule_next %d... \n");
 	int next;
 	int tamanio_buffer = sizeof(int);
 	void * buffer = malloc(tamanio_buffer);
@@ -55,7 +74,7 @@ int suse_schedule_next(){ //todo testear
 
 	serializar_int(buffer, &desp, msg);
 	enviar(socket_suse, cop_next_tid, tamanio_buffer, buffer);
-	log_info(logger, "Envie un mensaje (0) a SUSE. \n");
+//	log_info(logger, "Envie un mensaje (0) a SUSE. \n");
 	free(buffer);
 
 	t_paquete* received_packet = recibir(socket_suse);
@@ -63,11 +82,11 @@ int suse_schedule_next(){ //todo testear
 
 	if(received_packet->codigo_operacion == cop_next_tid){
 		next = deserializar_int(received_packet->data, &desplazamiento);
-		log_info(logger, "Recibi el next_tid %d. \n", next);
+//		log_info(logger, "Recibi el next_tid %d. \n", next);
 	}
 	else{
 		next = -1;
-		log_info("El codigo de operacion es incorrecto, deberia ser cop_next_tid y es %d", received_packet->codigo_operacion);
+//		log_info("El codigo de operacion es incorrecto, deberia ser cop_next_tid y es %d", received_packet->codigo_operacion);
 	}
 
 	liberar_paquete(received_packet);
@@ -82,7 +101,7 @@ int suse_close(int tid){
 	serializar_int(buffer, &desplazamiento, tid);
 	enviar(socket_suse, cop_close_tid, tamanio_buffer, buffer);
 	free(buffer);
-	log_info(logger, "Enviando a cerrar el thread %d. \n", tid);
+//	log_info(logger, "Enviando a cerrar el thread %d. \n", tid);
 
 	//SUSE me devuelve una respuesta indicando si logro cerrarlo o no
 	t_paquete* received_packet = recibir(socket_suse);
@@ -117,11 +136,11 @@ int suse_wait(int tid, char *sem_name){ //todo desde suse en la estructura agreg
 	void * buffer = malloc(tamanio_buffer);
 	int desplazamiento = 0;
 	serializar_int(buffer, &desplazamiento, tid);
-	serializar_valor(sem_name); //todo ver si el sem_name va con & o no
+	serializar_valor(sem_name,buffer,&desplazamiento); //todo ver si el sem_name va con & o no
 	enviar(socket_suse, cop_wait_sem, tamanio_buffer, buffer);
 	free(buffer);
-	log_info(logger, "Enviando a hacer un wait del sem %s", sem_name);
-	log_info(logger, "del thread %i. \n", tid);
+//	log_info(logger, "Enviando a hacer un wait del sem %s", sem_name);
+//	log_info(logger, "del thread %i. \n", tid);
 
 	//repuesta de suse que decremento el semaforo ok
 	t_paquete* received_packet = recibir(socket_suse);
@@ -139,10 +158,10 @@ int suse_signal(int tid, char *sem_name){
 	int desplazamiento = 0;
 
 	serializar_int(buffer, &desplazamiento, tid);
-	serializar_valor(sem_name); //todo ver si el sem_name va con & o no
+	serializar_valor(sem_name,buffer,&desplazamiento); //todo ver si el sem_name va con & o no
 	enviar(socket_suse, cop_signal_sem, tamanio_buffer, buffer);
 	free(buffer);
-	log_info(logger, "Enviando a hacer un signal del sem %s del thread %d. \n", sem_name, tid);
+//	log_info(logger, "Enviando a hacer un signal del sem %s del thread %d. \n", sem_name, tid);
 
 	t_paquete* received_packet = recibir(socket_suse);
 
@@ -156,7 +175,7 @@ int suse_signal(int tid, char *sem_name){
 
 
 hilolay_alumnos_configuracion get_configuracion_hilolay() {
-	log_info(logger,"Levantando archivo de configuracion de Hilolay alumnos \n");
+//	log_info(logger,"Levantando archivo de configuracion de Hilolay alumnos \n");
 	hilolay_alumnos_configuracion configuracion_hilolay;
 	t_config*  archivo_configuracion = config_create(HILOLAY_ALUMNO_CONFIG_PATH);
 	configuracion_hilolay.SUSE_IP = copy_string(get_campo_config_string(archivo_configuracion, "SUSE_IP"));
@@ -176,8 +195,8 @@ static struct hilolay_operations hiloops = {
 
 };
 
-void hilolay_init(void){
-	log_info(logger, "Ejecutando hilolay_init... \n");
+void hilolay_init(void){ //ESTA ESTA DEMAS. INIT ES DE HILOLAY, NO NUESTRA
+//	log_info(logger, "Ejecutando hilolay_init... \n");
 	init_internal(&hiloops);
 	conectar_con_suse();
 }
