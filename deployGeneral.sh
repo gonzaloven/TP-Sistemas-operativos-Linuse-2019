9f3c77de07c7562f1c02409f93f21a37fe420272
lopezjazmin <jaz.lopez84@gmail.com>
2019-12-07 00:06:55 -0300

    Agrego script para deploy general

---
+++ deployGeneral.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+mkdir workspace
+
+cd /home/utnso/workspace
+
+echo "Clonando hilolay..."
+git clone https://github.com/sisoputnfrba/hilolay
+
+echo "Clonando pruebas..."
+git clone https://github.com/sisoputnfrba/linuse-tests-programs
+
+echo "Clonando commons..."
+git clone https://github.com/sisoputnfrba/so-commons-library
+
+echo "Clonando tp..."
+git clone https://github.com/sisoputnfrba/tp-2019-2c-Los-Trapitos.git
+
+echo "Clonando proyecto con scripts de deploy..."
+git clone https://github.com/lopezjazmin/pruebasSO2019.git
+
+cd /home/utnso/workspace/so-commons-library
+
+echo "Ingrese password para instalar las commons..."
+sudo make install
+
+cd /home/utnso/workspace/hilolay
+echo "Ingrese password para instalar Hilolay..."
+sudo make
+sudo make install
+
+cd /home/utnso/workspace/tp-2019-2c-Los-Trapitos/utils_suse/
+echo "Ingrese password para instalar utils de SUSE..."
+sudo make clean
+sudo make
+sudo make all
+echo "Compiled with exit code $?"
+
+
+echo "Copiando MakeFile de pruebas..."
+cd /home/utnso/workspace/pruebasSO2019
+cp Makefile ../linuse-tests-programs
+
+echo "Copiando hilolay_alumnos..."
+cd /home/utnso/workspace/pruebasSO2019
+cp hilolay_alumnos ../linuse-tests-programs
+cd ..
+
+echo "Haciendo make a todo..."
+cd /home/utnso/workspace/linuse-tests-programs/
+sudo make clean
+sudo make
+sudo all
+echo "Compiled with exit code $?"
+
+cd /home/utnso/workspace/tp-2019-2c-Los-Trapitos
+sudo make clean
+sudo make
+sudo make all
+echo "Compiled with exit code $?"
+
+echo "Todo listo!!!!!"
+
